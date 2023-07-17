7c870fa3eaf8270251fcf88d65762d1aade0892d
artaff <art911191@gmail.com>
2023-07-17 19:12:49 +0700

    origin

---
+++ src/app/status-web/status-web.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-status-web',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './status-web.component.html',
+  styleUrls: ['./status-web.component.scss'],
+})
+export class StatusWebComponent {}
